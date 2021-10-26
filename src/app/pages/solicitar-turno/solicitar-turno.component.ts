18a825c6916b93c6a7207520182ada615f3364a8
Agustin Gaitan <gosagustingaitan@gmail.com>
2021-10-26 03:24:42 -0300

    15 dias agregados

--- src/app/pages/solicitar-turno/solicitar-turno.component.ts
+++ src/app/pages/solicitar-turno/solicitar-turno.component.ts
@@ -11,7 +11,13 @@ export class SolicitarTurnoComponent implements OnInit {
   especialistaClickeado : any;
   mostrarListaEspecialistas : boolean = false;
   horariosMostrar : any[] = [];
-  constructor() { }
+  fechasAMostrar : any[] = [];
+  posiblesTurnos : any;
+  constructor() {
+    let fecha = new Date();
+    console.log(fecha.getDay());
+
+  }
 
   ngOnInit(): void {
   }
@@ -25,6 +31,24 @@ export class SolicitarTurnoComponent implements OnInit {
   MostrarEspecialistaSeleccionado(event : any){
     this.especialistaClickeado = event;
     this.horariosMostrar = event.horarios;
+    let fechaPushear  = new Date().getDate();
+    //console.log(event.horarios);
+    let hoy = new Date();
+    let fecha = new Date();
+    for(let i = 0 ; i < 15 ; i++){
+
+      fecha.setDate(hoy.getDate() + 1);
+      hoy.setDate(hoy.getDate() + 1);
+      this.fechasAMostrar.push(fecha.toUTCString());
+    }
+    console.log(this.fechasAMostrar);
+    // for(let dia of event.horarios){
+      
+
+
+    // }
+
+   
        // this.mostrarListaEspecialistas = true;
   }
 
