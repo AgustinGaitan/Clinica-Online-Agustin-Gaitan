a3b0eaf8ce0128593c3cd90d22240b54d877e67a
Agustin Gaitan <gosagustingaitan@gmail.com>
2021-11-02 12:48:59 -0300

    Excel generado

--- src/app/pages/admin-usuarios/admin-usuarios.component.ts
+++ src/app/pages/admin-usuarios/admin-usuarios.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Administrador } from 'src/app/clases/administrador';
 import { FotoService } from 'src/app/services/foto.service';
 import { UserService } from 'src/app/services/user.service';
+import { Workbook } from 'exceljs';
+import * as fileSaver from 'file-saver';
+const ExcelJS = require('exceljs');
 
 @Component({
   selector: 'app-admin-usuarios',
@@ -15,6 +18,8 @@ export class AdminUsuariosComponent implements OnInit {
   formRegistro !: FormGroup;
   formData  : FormData = new FormData();
   mostrarRegistroAdmin = false;
+  
+  
 
   constructor(private fb : FormBuilder, private userService : UserService, private fotoService : FotoService) {
  
@@ -75,5 +80,37 @@ export class AdminUsuariosComponent implements OnInit {
 
   RegistrarAdmin(){
     this.mostrarRegistroAdmin = true;
+  }
+
+  Excel(){
+
+    const workbook = new ExcelJS.Workbook();
+    const sheet = workbook.addWorksheet('Usuarios');
+    let header = ["Nombre", "Apellido", "DNI", "Edad", "Correo", "Tipo de usuario"];
+    let headerRow = sheet.addRow(header);
+
+    let todosLosUsuarios = this.userService.todosLosPacientes.concat(this.userService.todosLosEspecialistas);
+    todosLosUsuarios = todosLosUsuarios.map((usuario : any)=>{
+        if(usuario?.especialidad){
+          usuario.tipo = 'Especialista';
+        }else{
+          usuario.tipo = 'Paciente';
+        }
+
+        return usuario;
+    });
+
+    for(let usuario of todosLosUsuarios){
+      let fila = [usuario.nombre, usuario.apellido, usuario.dni, usuario.edad, usuario.email, usuario.tipo];
+
+      sheet.addRow(fila);
+    }
+
+    let nombre = 'Clinica OnLine - Usuarios';
+
+    workbook.xlsx.writeBuffer().then((data : any) => {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fileSaver.saveAs(blob, nombre + '.xlsx');
+    });
   }
 }
